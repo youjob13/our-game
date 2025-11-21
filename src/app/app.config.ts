e0ad15422ae1a58d823aa7c39c49bafc02b97a8c
Danil Rodionov <Danil_Rodionov@epam.com>
2025-11-21 13:58:46 +0400

    chore: remove .prettierrc and update package.json for prettier configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,8 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes),
-  ],
+  providers: [provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection(), provideRouter(routes)],
 };
